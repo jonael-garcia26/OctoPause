e736d519566a7fb4e84304ac66ac385fa6d664b0
jonael-garcia26 <jonael.garcia@upr.edu>
2024-09-07 00:25:56 -0400

    Dont remember tbh

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-
+signal hit
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,3 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
