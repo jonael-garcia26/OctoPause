007ca1687a0372cde7fc9efc970ffe85b1957b9a
jonael-garcia26 <jonael.garcia@upr.edu>
2024-09-08 14:43:19 -0400

    puse la velocidad para cada mob

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 class_name EnemyClass
 
 @export var health = 100
+@export var velocity = 150
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -27,7 +27,7 @@ func _on_enemy_timer_timeout():
 	mob.rotation = direction
 
 	# Choose the velocity for the mob.
-	var velocity = Vector2(randf_range(150, 250), 0.0)
+	var velocity = Vector2(mob.velocity, 0.0)
 	mob.linear_velocity = velocity.rotated(direction)
 
 	# Spawn the mob by adding it to the Main scene.
