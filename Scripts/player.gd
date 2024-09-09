722c69399840f93e4d435d7f2efba6395e0ebb77
Fabiola Z. Torres Maldonado <fabiola.torres28@upr.edu>
2024-09-09 01:09:14 -0400

    Made pulpy shoot and kill enemies

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,6 +4,9 @@ extends Area2D
 @onready var currHealth: int = maxHealth
 
 var screen_size
+var inkBullet = preload("res://Scenes/ink_bullet.tscn")
+var shootTimer = 0
+var shootInterval = 0.3
 
 signal hit
 # Called when the node enters the scene tree for the first time.
@@ -16,6 +19,11 @@ func _process(delta: float) -> void:
 	var closestEnemy = get_closest_enemy()
 	if closestEnemy:
 		look_at(closestEnemy.position)
+		
+	shootTimer += delta
+	if shootTimer >= shootInterval:
+		shoot_ink_bullet()
+		shootTimer = 0
 
 
 func _on_body_entered(enemy: RigidBody2D) -> void:
@@ -48,3 +56,10 @@ func get_closest_enemy():
 				closest_enemy = enemy
 		
 	return closest_enemy
+
+
+func shoot_ink_bullet():
+	var bulletInstance = inkBullet.instantiate()
+	bulletInstance.position = $InkPoint.get_global_position()
+	bulletInstance.rotation_degrees = rotation_degrees
+	get_tree().get_root().add_child(bulletInstance)
